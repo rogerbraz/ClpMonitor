8c90a66d50cd740c94d151727a9228e4f00e8e05
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-04-02 21:47:25 -0300

    feat: implement chart

--- ClpWeb/src/app/dashboard/dashboard.component.ts
+++ ClpWeb/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,88 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { EChartsOption } from 'echarts';
+import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
+import { EnginePressures } from '../models/engine-pressure';
+import { DashboardService } from '../services/dashboard.service';
 
 @Component({
-  selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  selector: 'app-dashboard',
+  imports: [CommonModule, NgxEchartsDirective],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
+  providers: [
+    provideEcharts(),
+  ]
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  
+  data$: EnginePressures = [];
+  tempo: string[] = [];
+  pressao: number[] = [];
+  
 
+  constructor(private service: DashboardService) { }
+
+  ngOnInit(): void {
+    this.service.getEnginePressure().subscribe( response => {
+      response.forEach(data => {
+        this.tempo.push(String(data.tempo));
+        this.pressao.push(data.pressao);
+      });
+   });
+
+  }
+
+   
+  chartOption: EChartsOption = {
+     title: {
+       text: 'Temperature Change in the Coming Week'
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {},
+      toolbox: {
+        show: true,
+        feature: {
+          dataZoom: {
+            yAxisIndex: 'none'
+          },
+          dataView: { readOnly: false },
+          magicType: { type: ['line', 'bar'] },
+          restore: {},
+          saveAsImage: {}
+        }
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: this.tempo
+      },
+      yAxis: {
+        type: 'value',
+        axisLabel: {
+          formatter: '{value} °C'
+        }
+      },
+      series: [
+        {
+          name: 'Highest',
+          type: 'line',
+          data: this.pressao,
+          markPoint: {
+            data: [
+              { type: 'max', name: 'Max' },
+              { type: 'min', name: 'Min' }
+            ]
+          },
+          markLine: {
+            data: [{ type: 'average', name: 'Avg' }]
+          }
+        }
+      ]
+  };
+  
+  
 }
