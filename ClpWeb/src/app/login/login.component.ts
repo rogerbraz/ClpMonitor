850d50f308c124239859b298b3763fc82cc7aa33
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-04-02 21:48:53 -0300

    feat: config component like angular 17

--- ClpWeb/src/app/login/login.component.ts
+++ ClpWeb/src/app/login/login.component.ts
@@ -1,38 +1,38 @@
-import { Component } from '@angular/core';
-import { AuthService } from '../auth.service';
-import { PrimeNGConfig } from 'primeng/api';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css']
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   username: string = '';
   password: string = '';
 
   constructor(
-    private primengConfig: PrimeNGConfig,
-    private authService: AuthService,
+    // private primengConfig: PrimeNGConfig,
+    // private authService: AuthService,
     private router: Router // Adicione esta linha
 
   ) { }
 
   ngOnInit() {
     // Configurar o efeito de ondulação do PrimeNG
-    this.primengConfig.ripple = true;
+    // this.primengConfig.ripple = true;
   }
   
   login() {
-    this.authService.login(this.username, this.password).subscribe(response => {
-      // Armazenar o token JWT no localStorage ou sessionStorage
-      const token = response.token;
-      localStorage.setItem('token', token);
+    // this.authService.login(this.username, this.password).subscribe(response => {
+    //   // Armazenar o token JWT no localStorage ou sessionStorage
+    //   const token = response.token;
+    //   localStorage.setItem('token', token);
   
-      // Redirecionar para a página principal
-      this.router.navigate(['/dashboard']); // Supondo que 'router' seja o objeto do Angular Router
-    });
+    //   // Redirecionar para a página principal
+    // });
+    //   
+    this.router.navigate(['/home']); // Supondo que 'router' seja o objeto do Angular Router
   }
 }
