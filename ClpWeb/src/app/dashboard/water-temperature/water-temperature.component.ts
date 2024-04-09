4e4aa529c1b9142c3bb3ec6abcfa3c6635800996
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-04-08 23:05:20 -0300

    feat: water temperature component

---
+++ ClpWeb/src/app/dashboard/water-temperature/water-temperature.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../../services/dashboard.service';
+import { EChartsOption } from 'echarts';
+import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
+import { Temperatures } from '../../models/temperature';
+
+@Component({
+  selector: 'app-water-tempeture',
+  standalone: true,
+  imports: [ NgxEchartsDirective ],
+  templateUrl: './water-temperature.component.html',
+  styleUrl: './water-temperature.component.css',
+  providers: [
+    provideEcharts(),
+  ]
+})
+export class WaterTemperatureComponent implements OnInit {
+
+  chartOption: EChartsOption = {};
+  xAxis: string[] = [];
+  yAxis: number[] = [];
+  title: string = 'Temperatura Água (°C) X Tempo (s)';
+  
+
+  constructor(private service: DashboardService) { }
+
+  ngOnInit(): void {
+    this.service.getWaterTemperature().subscribe((response: Temperatures) => {
+      response.forEach((data) => {
+        this.xAxis.push(String(data.tempo));
+        this.yAxis.push(data.temperatura);
+      });
+
+      this.chartOption = this.service.initializeTemperatureGraph(this.xAxis, this.yAxis, this.title);
+    });
+    
+  }
+}
