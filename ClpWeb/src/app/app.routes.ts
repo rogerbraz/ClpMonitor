656b76ddf94d6647072dda2d6e4b51d195ba40f2
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-04-08 23:07:05 -0300

    feat: update routes

--- ClpWeb/src/app/app.routes.ts
+++ ClpWeb/src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { WaterPressureComponent } from './dashboard/water-pressure/water-pressure.component';
-import { WaterTempetureComponent } from './dashboard/water-tempeture/water-tempeture.component';
+import { WaterTemperatureComponent } from './dashboard/water-temperature/water-temperature.component';
 import { EnginePressureComponent } from './dashboard/engine-pressure/engine-pressure.component';
 import { EngineTemperatureComponent } from './dashboard/engine-temperature/engine-temperature.component';
 
@@ -12,7 +11,7 @@ export const routes: Routes = [
   { path: 'engine-pressure', component: EnginePressureComponent },
   { path: 'engine-temperature', component: EngineTemperatureComponent },
   { path: 'water-pressure', component: WaterPressureComponent },
-  { path: 'water-tempeture', component: WaterTempetureComponent },
+  { path: 'water-temperature', component: WaterTemperatureComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '/login', pathMatch: 'full' },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
