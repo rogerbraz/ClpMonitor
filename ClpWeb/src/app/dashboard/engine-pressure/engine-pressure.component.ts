4e7938b0fd36d97243349ff191001c350b177dc8
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-04-08 23:04:46 -0300

    feat: engine pressure component

---
+++ ClpWeb/src/app/dashboard/engine-pressure/engine-pressure.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../../services/dashboard.service';
+import { EChartsOption } from 'echarts';
+import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
+import { Pressures } from '../../models/pressure';
+
+
+@Component({
+  selector: 'app-engine-pressure',
+  standalone: true,
+  imports: [NgxEchartsDirective],
+  templateUrl: './engine-pressure.component.html',
+  styleUrl: './engine-pressure.component.css',
+  providers: [
+    provideEcharts(),
+  ]
+})
+export class EnginePressureComponent implements OnInit {
+
+  chartOption: EChartsOption = {};
+  xAxis: string[] = [];
+  yAxis: number[] = [];
+  title: string = 'Pressão Motor (psi) X Tempo (s)';
+
+
+  constructor(private service: DashboardService) { }
+
+  ngOnInit(): void {
+    this.service.getEnginePressure().subscribe((response: Pressures) => {
+      response.forEach((data) => {
+        this.xAxis.push(String(data.tempo));
+        this.yAxis.push(data.pressao);
+      });
+
+      this.chartOption = this.service.initializePressureGraph(this.xAxis, this.yAxis, this.title);
+    });
+
+  }
+}
