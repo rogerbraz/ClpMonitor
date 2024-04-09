e009e1fac68fb8c7633d6e155de8a19deeafe447
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-04-08 23:05:02 -0300

    feat: water pressure component

---
+++ ClpWeb/src/app/dashboard/water-pressure/water-pressure.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../../services/dashboard.service';
+import { EChartsOption } from 'echarts';
+import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
+import { Pressures } from '../../models/pressure';
+
+
+@Component({
+  selector: 'app-water-pressure',
+  standalone: true,
+  imports: [ NgxEchartsDirective ],
+  templateUrl: './water-pressure.component.html',
+  styleUrl: './water-pressure.component.css',
+  providers: [
+    provideEcharts(),
+  ]
+})
+export class WaterPressureComponent implements OnInit {
+
+  chartOption: EChartsOption = {};
+  xAxis: string[] = [];
+  yAxis: number[] = [];
+  title: string = 'Pressão Água (psi) X Tempo (s)';
+
+
+  constructor(private service: DashboardService) { }
+
+  ngOnInit(): void {
+    this.service.getWaterPressure().subscribe((response: Pressures) => {
+      response.forEach((data) => {
+        this.xAxis.push(String(data.tempo));
+        this.yAxis.push(data.pressao);
+      });
+
+      this.chartOption = this.service.initializePressureGraph(this.xAxis, this.yAxis, this.title);
+    });
+
+  }
+}
+
