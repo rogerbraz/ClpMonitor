ac22dde5d672b4cbf8acc309f329df8064140297
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-04-08 23:05:42 -0300

    feat: engine temperature component

---
+++ ClpWeb/src/app/dashboard/engine-temperature/engine-temperature.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../../services/dashboard.service';
+import { EChartsOption } from 'echarts';
+import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
+import { Temperatures } from '../../models/temperature';
+
+
+@Component({
+  selector: 'app-engine-temperature',
+  standalone: true,
+  imports: [NgxEchartsDirective],
+  templateUrl: './engine-temperature.component.html',
+  styleUrl: './engine-temperature.component.css',
+  providers: [
+    provideEcharts(),
+  ]
+})
+export class EngineTemperatureComponent implements OnInit {
+
+  chartOption: EChartsOption = {};
+  xAxis: string[] = [];
+  yAxis: number[] = [];
+  title: string = 'Temperatura Motor (°C) X Tempo (s)';
+  
+
+  constructor(private service: DashboardService) { }
+
+  ngOnInit(): void {
+    this.service.getEngineTemperature().subscribe((response: Temperatures) => {
+      response.forEach((data) => {
+        this.xAxis.push(String(data.tempo));
+        this.yAxis.push(data.temperatura);
+      });
+
+      this.chartOption = this.service.initializeTemperatureGraph(this.xAxis, this.yAxis, this.title);
+    });
+    
+  }
+}
