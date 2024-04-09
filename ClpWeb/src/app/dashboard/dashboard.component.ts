acc9651ed774c3439234380ec721d32edea697ea
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-04-08 23:06:25 -0300

    feat: general update

--- ClpWeb/src/app/dashboard/dashboard.component.ts
+++ ClpWeb/src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { EChartsOption } from 'echarts';
 import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
-import { EnginePressures } from '../models/engine-pressure';
 import { DashboardService } from '../services/dashboard.service';
 
 @Component({
@@ -17,72 +16,23 @@ import { DashboardService } from '../services/dashboard.service';
 })
 export class DashboardComponent implements OnInit {
   
-  data$: EnginePressures = [];
-  tempo: string[] = [];
-  pressao: number[] = [];
+  chartOption: EChartsOption = {};
+  xAxis: string[] = [];
+  yAxis: number[] = [];
   
 
   constructor(private service: DashboardService) { }
 
   ngOnInit(): void {
-    this.service.getEnginePressure().subscribe( response => {
-      response.forEach(data => {
-        this.tempo.push(String(data.tempo));
-        this.pressao.push(data.pressao);
-      });
-   });
+    // this.service.getEnginePressure().subscribe((response: any[]) => {
+    //   response.forEach((data: { tempo: any; pressao: number; }) => {
+    //     this.xAxis.push(String(data.tempo));
+    //     this.yAxis.push(data.pressao);
+    //   });
 
+    //   this.chartOption = this.service.initializeEnginePressureGraph(this.xAxis, this.yAxis);
+    // });
+    
   }
-
-   
-  chartOption: EChartsOption = {
-     title: {
-       text: 'Temperature Change in the Coming Week'
-      },
-      tooltip: {
-        trigger: 'axis'
-      },
-      legend: {},
-      toolbox: {
-        show: true,
-        feature: {
-          dataZoom: {
-            yAxisIndex: 'none'
-          },
-          dataView: { readOnly: false },
-          magicType: { type: ['line', 'bar'] },
-          restore: {},
-          saveAsImage: {}
-        }
-      },
-      xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: this.tempo
-      },
-      yAxis: {
-        type: 'value',
-        axisLabel: {
-          formatter: '{value} °C'
-        }
-      },
-      series: [
-        {
-          name: 'Highest',
-          type: 'line',
-          data: this.pressao,
-          markPoint: {
-            data: [
-              { type: 'max', name: 'Max' },
-              { type: 'min', name: 'Min' }
-            ]
-          },
-          markLine: {
-            data: [{ type: 'average', name: 'Avg' }]
-          }
-        }
-      ]
-  };
-  
   
 }
--- ClpWeb/src/app/home/home.component.ts
+++ ClpWeb/src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
