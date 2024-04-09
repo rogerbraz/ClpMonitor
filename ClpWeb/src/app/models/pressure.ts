26d0be62492a8b91579f5a6a0b0efd03a5690880
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-04-08 23:06:45 -0300

    feat: model pressure and temperature

---
+++ ClpWeb/src/app/models/pressure.ts
@@ -0,0 +1,6 @@
+export interface Pressure {
+    pressao: number,
+    tempo: number
+}
+
+export type Pressures = Array<Pressure>;
---
+++ ClpWeb/src/app/models/temperature.ts
@@ -0,0 +1,6 @@
+export interface Temperature {
+    temperatura: number,
+    tempo: number
+}
+
+export type Temperatures = Array<Temperature>;
